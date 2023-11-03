071516da1ba7000087d1a37f3e077a4dae4788bd
PelleDeOntwikkelaar <info@pelledeontwikkelaar.be>
2023-11-03 17:54:15 +0100

    intermediate commit

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,16 @@
-import { apiKey } from "./util.js";
+// import { apiKey as apiContent } from "./util.js";
+// import defaultapikey from "./util.js";
+// import * as util from "./util.js";
 
-import defaultapikey from "./util.js";
+//console.log(apiContent);
+//console.log(defaultapikey);
 
-import * as util from "./util.js";
+// console.log(util.default);
+// console.log(util.apiKey);
 
-console.log(apiKey);
-console.log(defaultapikey);
 
-console.log(util.default);
-console.log(util.apiKey);
+const userMessage = "Hello World!";
+
+console.log(userMessage);
+
+console.log(10 + 5);
