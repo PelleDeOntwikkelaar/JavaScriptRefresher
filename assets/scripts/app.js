50eadf9ee0af03cbd75094434387067dfdc4712d
PelleDeOntwikkelaar <info@pelledeontwikkelaar.be>
2023-11-03 17:19:31 +0100

    import export

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,5 +2,10 @@ import { apiKey } from "./util.js";
 
 import defaultapikey from "./util.js";
 
+import * as util from "./util.js";
+
 console.log(apiKey);
 console.log(defaultapikey);
+
+console.log(util.default);
+console.log(util.apiKey);
